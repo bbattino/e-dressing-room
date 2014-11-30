f86c1b4db0ff39d200ab70d54e840cd1d3059d5f
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2014-12-01 00:21:36 +0100

    Ajout d'une fenêtre de depart avec BorderLayout

---
+++ src/pactInitial/FenetreDepart.java
@@ -0,0 +1,35 @@
+package pactInitial;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class FenetreDepart extends JFrame {
+
+
+
+		private static final long serialVersionUID = 1L;
+
+		public FenetreDepart() {
+			setTitle("Bienvenue"); // Titre
+			setVisible(true); // affichage
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			// Utilisation de BorderLayout
+			Container contenu = getContentPane();
+			contenu.setLayout(new BorderLayout());
+			contenu.setBackground(new Color(255, 255, 255));
+
+			JLabel image = new JLabel(new ImageIcon("Data/bienvenue2.png"));
+
+			contenu.add(image, BorderLayout.CENTER);
+
+			this.pack();
+		}
+
+	}
+
+
