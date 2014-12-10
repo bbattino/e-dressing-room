537af1f9297baf89da421b1acb826fe46f9fc469
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2014-12-10 11:53:10 +0100

    Ajout de deux boutons dans a fenêtre de départ

--- src/pactInitial/FenetreDepart.java
+++ src/pactInitial/FenetreDepart.java
@@ -9,13 +9,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class FenetreDepart extends JFrame implements ActionListener{
 
 
 
 		private static final long serialVersionUID = 1L;
+		private JButton commencerButton = new JButton("Commencer");
+		private JButton creditsButton = new JButton("A propos");
+		private JPanel bouttons = new JPanel();
 
 		public FenetreDepart() {
 			setTitle("Bienvenue"); // Titre
@@ -28,8 +33,15 @@ public class FenetreDepart extends JFrame implements ActionListener{
 			contenu.setBackground(new Color(255, 255, 255));
 
 			JLabel image = new JLabel(new ImageIcon("data/bienvenue.png"));
+			
+			//Ajouts des bouttons sur buttons
+			
+			bouttons.add(commencerButton);
+			bouttons.add(creditsButton);
 
+			//Ajouts sur le ContentPane
 			contenu.add(image, BorderLayout.CENTER);
+			contenu.add(bouttons,BorderLayout.PAGE_END);
 
 			this.pack();
 		}
