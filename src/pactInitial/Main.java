3ce2a64647e1025411c5a758e4d834fff2d7d505
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2014-12-15 17:07:44 +0100

    Le programme joue un son au démarage (pour l'instant musique de Super Mario)

--- src/pactInitial/Main.java
+++ src/pactInitial/Main.java
@@ -1,9 +1,13 @@
 package pactInitial;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		java.net.URL urlSon = Main.class.getResource("superMario.wav");
+		java.applet.AudioClip son = java.applet.Applet.newAudioClip(urlSon);
+		son.play();
 		new FenetreDepart();
 
 	}
