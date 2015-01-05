52260fa9da3ffc0d2d8de9cd0ec0cc30c82694c9
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-01-05 08:27:28 +0100

    Ajout d'une classe LecteurAudio qui permet de lire un fichier audio .wav

---
+++ src/audio/LecteurAudio.java
@@ -0,0 +1,13 @@
+package audio;
+
+
+public class LecteurAudio {
+	
+	public LecteurAudio(String TitreAudio){
+		
+		java.net.URL urlSon = LecteurAudio.class.getResource(TitreAudio);
+		java.applet.AudioClip son = java.applet.Applet.newAudioClip(urlSon);
+		son.play();
+	}
+
+}
