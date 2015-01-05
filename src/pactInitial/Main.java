aa1d189a7b9985148260a4e104ceee60ad01e280
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-01-05 08:30:11 +0100

    Modification du main en conséquence de la nouvelle classe LecteurAudio

--- src/pactInitial/Main.java
+++ src/pactInitial/Main.java
@@ -1,13 +1,13 @@
 package pactInitial;
 
+import audio.LecteurAudio;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		java.net.URL urlSon = Main.class.getResource("superMario.wav");
-		java.applet.AudioClip son = java.applet.Applet.newAudioClip(urlSon);
-		son.play();
+		new LecteurAudio("superMario.wav");
 		new FenetreDepart();
 
 	}
