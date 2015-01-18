4ac67221c29307a3d539666267b945f0f27c9fcb
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-01-18 11:46:42 +0100

    Ajout d'un bouton test pour la fentre de test de GridBagLayout

--- src/fenetres/FenetreGridBag.java
+++ src/fenetres/FenetreGridBag.java
@@ -1,19 +1,22 @@
 package fenetres;
 
+import java.awt.Container;
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class FenetreGridBag extends JFrame {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	private Container contenu = getContentPane();
+	
 	
 	public FenetreGridBag(){
 		setVisible(true);
-		getContentPane().setLayout(new GridBagLayout());
+		contenu.setLayout(new GridBagLayout());
+		contenu.add(new JButton("ok"));
 		
 	}
 
