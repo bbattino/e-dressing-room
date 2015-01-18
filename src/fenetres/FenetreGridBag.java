d28a04c1cdcdcb84580de19eb4be23283ade47a0
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-01-18 01:26:28 +0100

    Ajout d'une nouvelle fenêtre pour tester gridBagLayout

---
+++ src/fenetres/FenetreGridBag.java
@@ -0,0 +1,23 @@
+package fenetres;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+
+public class FenetreGridBag extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public FenetreGridBag(){
+		setVisible(true);
+		getContentPane().setLayout(new GridBagLayout());
+		
+	}
+
+	
+	
+
+}
