0e115de400084e998296c8e43ea5141396f865ef
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-01-28 13:20:23 +0100

    contrainte GridBagLayout [modification minime]

--- src/fenetres/FenetreGridBag.java
+++ src/fenetres/FenetreGridBag.java
@@ -1,6 +1,7 @@
 package fenetres;
 
 import java.awt.Container;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
@@ -14,8 +15,12 @@ public class FenetreGridBag extends JFrame {
 	
 	
 	public FenetreGridBag(){
+		
 		setVisible(true);
 		contenu.setLayout(new GridBagLayout());
+		GridBagConstraints constrains = new GridBagConstraints();
+		constrains.gridx=0;
+		constrains.gridy=0;// Initialisation de la grille en 0,0
 		contenu.add(new JButton("ok"));
 		contenu.add(new JButton("ok2"));
 		setSize(700,300);
