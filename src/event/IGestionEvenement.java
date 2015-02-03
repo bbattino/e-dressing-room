444c6a1172a6e3cefca986b1df37affdac00e3be
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-03 16:49:35 +0100

    [Integration] Ajout d'une interface de conception pour la gestion d'evênements

---
+++ src/event/IGestionEvenement.java
@@ -0,0 +1,11 @@
+package event;
+
+public interface IGestionEvenement {
+	
+	public void getPositionMain();
+	public boolean MainSurBoutton();
+	public void launchTimer();
+	public void stopTimer();
+	public void action();
+
+}
