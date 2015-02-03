67d8292adaf8d7dd4251fac26b2c2b9c83ae5645
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-03 17:31:32 +0100

    [Integration] Ajout d'un timerTask

--- src/event/ProducteurEvent.java
+++ src/event/ProducteurEvent.java
@@ -4,12 +4,26 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 
 public class ProducteurEvent implements IGestionEvenement{
 	
 	private ArrayList<JButton> boutons;
+	private TimerTask timertask = new TimerTask() {
+		
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("3 secondes écoulées");
+			
+		}
+	};
 
 	public Point getPositionMain() {
 		return null;
@@ -36,13 +50,11 @@ public class ProducteurEvent implements IGestionEvenement{
 	}
 
 	public void launchTimer() {
-		// TODO Auto-generated method stub
-		
+		this.timertask.run();
 	}
 
 	public void stopTimer() {
-		// TODO Auto-generated method stub
-		
+		this.timertask.cancel();
 	}
 
 	public void action() {
