2c69355bf89211e84df24ceff0f97398a015cbe5
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-08 11:13:02 +0100

    [integration)Ajout d'une classe abstraite Fenetre contenant une méthode pour activer un bouton des qu'on y reste une seconde

--- src/fenetres/Fenetre.java
+++ src/fenetres/Fenetre.java
@@ -1,24 +1,71 @@
 package fenetres;
 
-import java.awt.Container;
-import java.awt.event.ActionEvent;
+import java.awt.Rectangle;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.*;;
+import javax.swing.*;
+
+import event.MainDansLaZoneEventProducer;
+import event.TrucKinect;
+import event.MainDansLaZoneEventProducer.IMainDansLaZoneListener;
 
 public abstract class Fenetre extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private ArrayList<JButton> listeBoutons = new ArrayList<JButton>();
-	private JPanel bouttons = new JPanel();
-	private Container contenu = getContentPane();
 
+	public void addHandListener(final JButton bouton) {
+		final MainDansLaZoneEventProducer eventProducer = new MainDansLaZoneEventProducer(new TrucKinect(bouton),
+				new Rectangle(bouton.getLocationOnScreen(), bouton.getSize()), 1000, 100); // buttonBounds
+																							// ce
+																							// sont
+																							// les
+																							// bounds
+																							// du
+																							// bouton
+																							// (à
+																							// priori
+																							// dans
+																							// l'écran)
 
+		// alternative 1 : classe anonyme
+		eventProducer.addListener(new IMainDansLaZoneListener() {
+			public void mainDansLaZone(MainDansLaZoneEventProducer.Type type) {
+				switch (type) {
+				case ENTER: // on entre la zone
+					System.out.println("enter");
+					bouton.getModel().setArmed(true);
+					bouton.getModel().setPressed(true);
+					break;
+				case HIT: // si la main est restée 3 secondes
+					System.out.println("hit");
+					bouton.getModel().setArmed(false);
+					bouton.getModel().setPressed(false);
+					bouton.doClick(); // je clique le bouton
+					break;
+				case EXIT: // si la main sort (attention avec ce que j'ai fait,
+							// elle sort toujours, même si elle est restée 3
+							// secondes...
+					System.out.println("exit");
+					bouton.getModel().setArmed(false);
+					bouton.getModel().setPressed(false);
+					break;
+				}
+			}
+		});
 
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+		// ou alors alternative 2 : classe concrete Listener
+		// eventProducer.addListener(new Listener(bouton));
+
+		eventProducer.start();
+
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				eventProducer.stop();
+			}
+		});
 
 	}
-
 }
