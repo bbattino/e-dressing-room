c21bfeab4f19cf3912e846d29b4801dc4d6c3a5a
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-17 10:01:06 +0100

    Modification de la JOptionPane personnalisée BIS (oubli du commit du fichier java JOptionPanePerso.java dans le commit précedent)

--- src/fenetres/JOptionPanePerso.java
+++ src/fenetres/JOptionPanePerso.java
@@ -1,30 +1,37 @@
 package fenetres;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+//import javax.swing.JTextArea;
 
 public class JOptionPanePerso extends Fenetre {
 
 static final long serialVersionUID = 1L;
-private JTextArea texte;
+//private JTextArea texte;
 private JPanel boutonsPanel = new JPanel();
 private ArrayList<JButton> boutonListe=new ArrayList<JButton>();
 private ArrayList<Runnable> boutonActionListe= new ArrayList<Runnable>();
+private JLabel image;
 
-public JOptionPanePerso(String Message, String[] boutonsTitre, Runnable[] boutonsAction){
-	this.texte=new JTextArea(Message);
+public JOptionPanePerso(String Message,String imagePath, String[] boutonsTitre, Runnable[] boutonsAction){
+	//this.texte=new JTextArea(Message);
+	//texte.setEditable(false);
+	setLocation(600, 400);
+	image = new JLabel(new ImageIcon(imagePath));
 	Container contenu = getContentPane();
 	contenu.setLayout(new BorderLayout());
 	setVisible(true);
 	setTitle("message");
-	contenu.add(texte,BorderLayout.PAGE_START);
+	//contenu.add(texte,BorderLayout.PAGE_START);
 	boutonsPanel.setLayout(new FlowLayout());
 	for(int i=0;i<boutonsTitre.length;i++){
 		JButton bouton = new JButton(boutonsTitre[i]);
@@ -34,7 +41,9 @@ public JOptionPanePerso(String Message, String[] boutonsTitre, Runnable[] bouton
 		boutonActionListe.add(boutonsAction[i]);
 		addHandListener(bouton);
 	}
+	boutonsPanel.setBackground(new Color(255,255,255));
 	contenu.add(boutonsPanel,BorderLayout.PAGE_END);
+	contenu.add(image,BorderLayout.LINE_START);
 	pack();
 }
 
