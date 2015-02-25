27b595feb2a9c4b629d8033fe344cf78de177153
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-25 23:35:46 +0100

    Grosse modification de la classe FenetreEntreeNom : le clavier est mis sous la forme d'un clavier azerty & la mise en majuscule est automatique en début de mot et après un espace

--- src/fenetres/FenetreEntreeNom.java
+++ src/fenetres/FenetreEntreeNom.java
@@ -1,6 +1,7 @@
 package fenetres;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
@@ -28,84 +29,58 @@ public class FenetreEntreeNom extends Fenetre {
 		Main.setCurentFenetre(this);
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setUndecorated(true);
+		
+		JButton boutonVide1 = new JButton();
+		JButton boutonVide2 = new JButton();
+		boutonVide1.setBackground(new Color(255,255,255));
+		boutonVide2.setBackground(new Color(255,255,255));
+		
+
+		
 		alphabet.add(new JButton("a"));
-		alphabet.add(new JButton("b"));
-		alphabet.add(new JButton("c"));
-		alphabet.add(new JButton("d"));
+		alphabet.add(new JButton("z"));
 		alphabet.add(new JButton("e"));
+		alphabet.add(new JButton("r"));
+		alphabet.add(new JButton("t"));
+		alphabet.add(new JButton("y"));
+		alphabet.add(new JButton("u"));
+		alphabet.add(new JButton("i"));
+		alphabet.add(new JButton("o"));
+		alphabet.add(new JButton("p"));
+		
+		alphabet.add(new JButton("q"));
+		alphabet.add(new JButton("s"));
+		alphabet.add(new JButton("d"));
 		alphabet.add(new JButton("f"));
 		alphabet.add(new JButton("g"));
 		alphabet.add(new JButton("h"));
-		alphabet.add(new JButton("i"));
 		alphabet.add(new JButton("j"));
 		alphabet.add(new JButton("k"));
 		alphabet.add(new JButton("l"));
 		alphabet.add(new JButton("m"));
-		alphabet.add(new JButton("n"));
-		alphabet.add(new JButton("o"));
-		alphabet.add(new JButton("p"));
-		alphabet.add(new JButton("q"));
-		alphabet.add(new JButton("r"));
-		alphabet.add(new JButton("s"));
-		alphabet.add(new JButton("t"));
-		alphabet.add(new JButton("u"));
-		alphabet.add(new JButton("v"));
+
 		alphabet.add(new JButton("w"));
 		alphabet.add(new JButton("x"));
-		alphabet.add(new JButton("y"));
-		alphabet.add(new JButton("z"));
+		alphabet.add(new JButton("c"));
+		alphabet.add(new JButton("v"));
+		alphabet.add(new JButton("b"));
+		alphabet.add(new JButton("n"));
 
-		alphabet.add(new JButton("A"));
-		alphabet.add(new JButton("B"));
-		alphabet.add(new JButton("C"));
-		alphabet.add(new JButton("D"));
-		alphabet.add(new JButton("E"));
-		alphabet.add(new JButton("F"));
-		alphabet.add(new JButton("G"));
-		alphabet.add(new JButton("H"));
-		alphabet.add(new JButton("I"));
-		alphabet.add(new JButton("J"));
-		alphabet.add(new JButton("K"));
-		alphabet.add(new JButton("L"));
-		alphabet.add(new JButton("M"));
-		alphabet.add(new JButton("N"));
-		alphabet.add(new JButton("O"));
-		alphabet.add(new JButton("P"));
-		alphabet.add(new JButton("Q"));
-		alphabet.add(new JButton("R"));
-		alphabet.add(new JButton("S"));
-		alphabet.add(new JButton("T"));
-		alphabet.add(new JButton("U"));
-		alphabet.add(new JButton("V"));
-		alphabet.add(new JButton("W"));
-		alphabet.add(new JButton("X"));
-		alphabet.add(new JButton("Y"));
-		alphabet.add(new JButton("Z"));
-
-		alphabet.add(new JButton("-"));
-		alphabet.add(new JButton("0"));
-		alphabet.add(new JButton("1"));
-		alphabet.add(new JButton("2"));
-		alphabet.add(new JButton("3"));
-		alphabet.add(new JButton("4"));
-		alphabet.add(new JButton("5"));
-		alphabet.add(new JButton("6"));
-		alphabet.add(new JButton("7"));
-		alphabet.add(new JButton("8"));
-		alphabet.add(new JButton("9"));
-
-		alphabet.add(new JButton("é"));
-		alphabet.add(new JButton("è"));
-		alphabet.add(new JButton("ê"));
-		alphabet.add(new JButton("ë"));
-		alphabet.add(new JButton("ç"));
-
-		alphabet.add(new JButton(" "));
+		alphabet.add(new JButton("Espace"));
 		alphabet.add(new JButton("DEL"));
 
-		alphabetPanel.setLayout(new GridLayout(6, 14));
-		for (int i = 0; i < alphabet.size(); i++)
+		alphabetPanel.setLayout(new GridLayout(3,10));
+		for (int i = 0; i < 10; i++)
 			alphabetPanel.add(alphabet.get(i));
+		for(int i=10;i<20;i++)
+			alphabetPanel.add(alphabet.get(i));
+		alphabetPanel.add(boutonVide1);
+		for (int i=20;i<26;i++)
+			alphabetPanel.add(alphabet.get(i));
+		alphabetPanel.add(boutonVide2);
+		for (int i=26; i<28;i++)
+			alphabetPanel.add(alphabet.get(i));
+
 
 		this.setVisible(true);
 		getContentPane().setLayout(new BorderLayout());
@@ -164,13 +139,30 @@ public class FenetreEntreeNom extends Fenetre {
 			if (e.getSource() == button) {
 				if (button.getText().equals("DEL")) {
 					deleteChar();
-				} else {
+				} 
+				else if(button.getText().equals("Espace")){
+					name+=" ";
+					getContentPane().remove(texte);
+					texte = new JTextArea("\n \t \t Votre nom est : " + name + " \n\n");
+					getContentPane().add(texte,BorderLayout.PAGE_START);
+					setSize(1001, 800);	this.setExtendedState(Frame.MAXIMIZED_BOTH);
+					
+					
+				}
+				else if(name.equals("")|| name.substring(name.length()-1, name.length()).equals(" ")){
+					name = name + button.getText().toUpperCase();
+					getContentPane().remove(texte);
+					texte = new JTextArea("\n \t \t Votre nom est : " + name + " \n\n");
+					getContentPane().add(texte,BorderLayout.PAGE_START);
+					setSize(1001, 800);	this.setExtendedState(Frame.MAXIMIZED_BOTH);
+				}
+				
+				else {
 					name = name + button.getText();
 					getContentPane().remove(texte);
 					texte = new JTextArea("\n \t \t Votre nom est : " + name + " \n\n");
 					getContentPane().add(texte,BorderLayout.PAGE_START);
 					setSize(1001, 800);	this.setExtendedState(Frame.MAXIMIZED_BOTH);
-
 					
 				}
 				return;
