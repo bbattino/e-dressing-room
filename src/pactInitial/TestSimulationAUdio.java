d091447d623e52c5f93d9287667799ca6ecd54c5
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-02-26 10:36:22 +0100

    [integration audio] amélioration visuelle de la fenêtre de test d'integration audio. (Le test est concluant)

--- src/pactInitial/TestSimulationAUdio.java
+++ src/pactInitial/TestSimulationAUdio.java
@@ -1,8 +1,9 @@
 package pactInitial;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -13,8 +14,7 @@ import javax.swing.JPanel;
 
 import fenetres.FenetreDepart;
 
-
-public class TestSimulationAUdio extends JFrame implements ActionListener{
+public class TestSimulationAUdio extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private ArrayList<JButton> alphabet = new ArrayList<JButton>();
@@ -23,38 +23,28 @@ public class TestSimulationAUdio extends JFrame implements ActionListener{
 	public TestSimulationAUdio() {
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setUndecorated(true);
-		
-		
-
-		for(int i=0; i<60;i++){
-			alphabet.add(new JButton(""+i));
+		alphabetPanel.setLayout(new GridLayout(10,10));
+		for (int i = 0; i < 60; i++) {
+			alphabet.add(new JButton("" + i));
 			alphabetPanel.add(alphabet.get(i));
 		}
-		
 
 		this.setVisible(true);
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(alphabetPanel, BorderLayout.CENTER);
 
-		
-
-		
-		for (JButton bouton : alphabet){
+		for (JButton bouton : alphabet) {
 			bouton.addActionListener(this);
-			}
-		
-
+		}
 	}
-
-	
 
 	public void actionPerformed(ActionEvent e) {
 		JButton bouton = (JButton) e.getSource();
-		int numero= Integer.parseInt(bouton.getText());
+		int numero = Integer.parseInt(bouton.getText());
 		Main.actionEventAudio(numero);
-		
+
 	}
-		
+
 	public static void main(String[] args) {
 		new FenetreDepart();
 		new TestSimulationAUdio();
