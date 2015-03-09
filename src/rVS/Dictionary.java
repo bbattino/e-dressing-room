d12b8ca18110dc1e52762e9ec1ab93e2be32da95
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-03-09 07:50:47 +0100

    [integration RVS] import et modification des classes de RVS

---
+++ src/rVS/Dictionary.java
@@ -0,0 +1,80 @@
+package rVS;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import mFCC_DTW.Mot;
+
+
+public class Dictionary {
+	private Hashtable<Mot, String> dataBase; // Notre base de données : les mots et leur chaine de caractères
+	
+	/* Constructeur */
+	public Dictionary(){
+		Hashtable<Mot, String> initialisation = new Hashtable<Mot, String>();
+		
+		setString(new Mot("fileName1"), "fileName1"); // Devra être répété pour chaque mot de la base de donnée, chaque enregistrement
+		
+		this.dataBase = initialisation;
+	}
+	
+	/* Compare un enregistrement à tous les mots de la base de données */
+	public String compareAll(Mot other){
+		double ref = Float.POSITIVE_INFINITY; // Variable qui sert de reference pour calculer la distance minimale, on l'initialise la plus grande possible
+		double distCurrent = 0;
+		String result = ""; // Le futur résultat retourné
+		
+		/* Comparaison avec tous les mots de dataBase */
+		Enumeration<Mot> enumMot = dataBase.keys();
+		while(enumMot.hasMoreElements()){
+			Mot motCurr = enumMot.nextElement(); // On récupère le prochain mot
+			distCurrent = motCurr.calculDistanceMot(other); // On calcule sa distance au mot voulu
+			
+			/* Mmet à jour distance afin d'obtenir la valeur minimale */
+			if(distCurrent < ref){
+				ref = distCurrent;
+				result = getString(motCurr);
+			}
+		}
+		
+		return result;
+	}
+	
+	/* Renvoie la chaine de caractère associé à un mot de la base de données */
+	public String getString(Mot mot){
+		String res = dataBase.get(mot);
+		return res;
+	}
+	
+	/* Permet d'initialiser la base de données */
+	public void setString(Mot mot, String nom){
+		dataBase.put(mot,  nom);
+	}
+	
+	/* Convertit le résultat (string) en instruction pour l'interface graphique */
+	public int convertToInterface(String name){
+		int res;
+		switch(name){
+		case "commencer" :
+			res = 0; break;
+		case "retour" :
+			res = 10; break;
+		case "quitter" :
+			res = 2; break;
+		case "3" :
+			res = 5; break;
+		case "pull" :
+			res = 13; break;
+		case "suivant" :
+			res = 17; break;
+		case "essayer" :
+			res = 20; break;
+		case "ok" : 
+			res = 22; break;
+		default :
+			res = -1; break; // Insérer ici un code d'erreur
+		}
+		
+		return res;
+	}
+}
---
+++ src/rVS/MainTest.java
@@ -0,0 +1,19 @@
+package rVS;
+
+import mFCC_DTW.Mot;
+
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// Enregistre le son émis par l'utilisateur
+		Mot motUtilisateur = new Mot("nomDuSon");
+		
+		// Initialise le dictionnaire
+		Dictionary dictionary = new Dictionary();
+		
+		String result = dictionary.compareAll(motUtilisateur);
+		int resInterface = dictionary.convertToInterface(result);
+	}
+
+}
