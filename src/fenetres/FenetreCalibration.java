78efb73c2a7a8501affd8a51f4294ae303a3dcd9
Battino Benjamin <benjamin.battino@telecom-paristech.fr>
2015-03-14 23:58:58 +0100

    [integration Kinect] ajout des méthodes codées de la fenêtre de calibration

--- src/fenetres/FenetreCalibration.java
+++ src/fenetres/FenetreCalibration.java
@@ -7,32 +7,66 @@ import java.io.InputStreamReader;
 
 import javax.swing.JFrame;
 
+import audio.LecteurAudio;
 import pactInitial.ICalibration;
 
 public class FenetreCalibration extends JFrame implements ICalibration{
 
 	private static final long serialVersionUID = 1L;
-	private float xHaut,yHaut,xBas,yBas;
+	private float xMax,yMax,xMin,yMin;
+	private int xMaxEcran,yMaxEcran;
+	float ax,ay,bx,by;
 	private String path;
 
 
 	@Override
-	public float[] getFloatSeparated(String string) {
+	public float[] getFloatSeparated(String s) {
 		
 		
-		
-		return null;
+			String xString="";
+			String yString="";
+			String CurentWord = "";
+			
+			for (int i=0; i< s.length();i++){
+				if(s.substring(i,i+1).equals(" ")){
+					xString=CurentWord;
+					yString=s.substring(1+i,s.length());
+				
+			}else{
+				CurentWord+=s.substring(i,i+1);
+			}
+				
+			}
+			float x= Float.parseFloat(xString);
+			float y= Float.parseFloat(yString);
+			float[] tab = {x,y};
+			return tab;
 	}
 
 	@Override
 	public void calculateCoeff() {
-		// TODO Auto-generated method stub
-		
+		this.ax=(float) (-(xMaxEcran+0.0)/(xMax-xMin));
+		this.bx=(float) -xMax*this.ax;
+		this.ay=(float) (-(yMaxEcran+0.0)/(yMax-yMin));
+		this.by=(float) -yMax*this.ay;
 	}
 
 	@Override
 	public void waitForHand() {
-		// TODO Auto-generated method stub
+		new LecteurAudio(""); // mettez en haut gauche jusqu'au bip
+		try {wait(3000);} 
+		catch (InterruptedException e) {e.printStackTrace();}
+		float[] position = getFloatSeparated(lectureFichier());
+		xMin=position[0];yMin=position[1];
+		
+		new LecteurAudio("");//BIP
+		new LecteurAudio("");// mettez en bas droite jusqy'au bip
+		try {wait(3000);} 
+		catch (InterruptedException e) {e.printStackTrace();}
+		position = getFloatSeparated(lectureFichier());
+		xMax=position[0];yMax=position[1];
+		
+		
 		
 	}
 
@@ -49,6 +83,14 @@ public class FenetreCalibration extends JFrame implements ICalibration{
 			System.out.println(e.toString());
 		}	
 		return coordonates;
+	}
+	
+	public static void main(String[] args){
+		String s = "-1.173543 1.1111";
+		FenetreCalibration fc=new FenetreCalibration();
+		System.out.println(fc.getFloatSeparated(s)[0]);
+		System.out.println(fc.getFloatSeparated(s)[1]);
+
 	}
 
 }
